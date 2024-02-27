19a8ad90026db553adf8e5c5ac283b5bff9a94fb
varshini18558 <71538570+varshini18558@users.noreply.github.com>
2024-02-28 04:02:07 +0530

    Add files via upload

---
+++ project js/main.js
@@ -0,0 +1,17 @@
+// $('#pictureTarifa, #tarifaHorarios').collapse('hide'); 
+// $('input[name="radios"]').change( function() {
+//     if ($('#pictureTar').is(":checked")){
+//       $('#pictureTarifa').collapse('show');
+//     } else {
+//       $('#pictureTarifa').collapse('hide');
+//     } 
+//     if ($('#tarifaHor').is(":checked")){
+//       $('#tarifaHorarios').collapse('show');
+//     } else {
+//       $('#tarifaHorarios').collapse('hide');
+//     } 
+//     if ($('#radio3').is(":checked")){
+//       $('#pictureTarifa').collapse('hide');
+//       $('#tarifaHorarios').collapse('hide');
+//     }
+//   });
